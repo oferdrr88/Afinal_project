4a93444d1e773d2ac68597790c757f6d61eb74e7
oferd845 <oferd845@gmail.com>
2023-02-01 06:26:16 +0200

    start_server

---
+++ backend/server.js
@@ -0,0 +1,25 @@
+// const { request } = require('./app');
+// const express = require('express');
+// const app = express();
+// // const app = require('./app');
+// const connectDatabase = require('./config/database');
+
+// const dotenv = require('dotenv');
+
+// // // set config
+// config({ path: 'backend/config/config.env' });
+
+// // // conect database
+// connectDatabase();
+
+// listen(process.env.PORT, () => {
+//     console.log(`server start on port,${process.env.PORT} in ${process.env.NODE_ENV} mode `);
+// });
+// module.exports = app;
+
+const app = require('./app');
+const dotenv = require('dotenv');
+dotenv.config({ path: 'backend/config/config.env' });
+app.listen(process.env.PORT, () => {
+    console.log(`server start on port,${process.env.PORT} in ${process.env.NODE_ENV} mode `);
+});
