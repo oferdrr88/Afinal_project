13da42c1d3a4e93b579f581fb2416c098825c800
oferd845 <oferd845@gmail.com>
2023-02-03 01:00:38 +0200

    fix_single_deletק

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -1,6 +1,6 @@
 const Product = require('../models/products');
 
-//create new.product  => /api/v1/product/new
+//create new.product  => admin/api/v1/product/new
 
 exports.newProduct = async (req, res, next) => {
     const product = await Product.create(req.body);
@@ -36,5 +36,43 @@ exports.getSingleProduct = async (req, res, next) => {
     res.status(200).json({
         success: true,
         product,
+    });
+};
+
+// update product => /admin/api/v1/product/:id
+
+exports.updateProduct = async (req, res, next) => {
+    let product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found',
+        });
+    }
+    product = await Product.findByIdAndUpdate(req.params.id, req.body, {
+        new: true,
+        runValidators: true,
+        useFindAndModify: false,
+    });
+    res.send(200).json({
+        success: true,
+        product,
+    });
+};
+//delete product => api/v1/admin/product/:id
+
+exports.deleteProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found',
+        });
+    }
+    await Product.remove();
+
+    res.status(200).json({
+        success: true,
+        message: 'Product is Deleted',
     });
 };
---
+++ backend/utils/errorHandele.js
@@ -0,0 +1,12 @@
+// class error
+
+// class ErrorHandele extends Error {
+//     constructor(massage, stotusCode) {
+//         super(massage);
+//         this.stotusCode = stotusCode;
+
+//         Error.captureStackTrace(this, this.constructor);
+//     }
+// }
+
+// module.exports = ErrorHandele;
