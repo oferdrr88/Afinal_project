bac1be088262d5645870d9190b7f48e5d19de011
oferd845 <oferd845@gmail.com>
2023-02-02 23:16:26 +0200

    getSingleProduct

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -21,3 +21,20 @@ exports.getProducts = async (req, res, next) => {
         products,
     });
 };
+
+// get single product detauls => /api/v1/product/:id
+
+exports.getSingleProduct = async (req, res, next) => {
+    const product = await Product.findById(req.params.id);
+
+    if (!product) {
+        return res.status(404).json({
+            success: false,
+            message: 'Product not found',
+        });
+    }
+    res.status(200).json({
+        success: true,
+        product,
+    });
+};
--- backend/routes/products.js
+++ backend/routes/products.js
@@ -2,9 +2,10 @@ const express = require('express');
 
 const router = express.Router();
 
-const { getProducts, newProduct } = require('../controllers/productController');
+const { getProducts, newProduct, getSingleProduct } = require('../controllers/productController');
 
 router.route('/products').get(getProducts);
+router.route('/products/:id').get(getSingleProduct);
 
 router.route('/products/new').post(newProduct);
 
