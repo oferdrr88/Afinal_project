6f9dd2ea72838e066bbe6d19700744e4a4b0b75b
oferd845 <oferd845@gmail.com>
2023-02-07 19:45:06 +0200

    schemauser

---
+++ backend/controllers/authConroler.js
@@ -0,0 +1,19 @@
+const User = require('../models/user');
+const Errorhandler = require('../utils/errorHandeler');
+const catchAsyncError = require('../middlelwares/catchAsyncErrors');
+
+//Register auser => /api/v1/register
+
+exports.registerUser = catchAsyncError(async (req, res, next) => {
+    const { name, email, password } = req.body;
+
+    const user = await User.create({
+        name,
+        email,
+        password,
+        avatar: {
+            public_id: '',
+            url: '',
+        },
+    });
+});
